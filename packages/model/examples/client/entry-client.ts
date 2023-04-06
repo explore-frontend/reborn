4d52f7bc105ce47033b638b9273c80ff3e92c8d5
Skyline Yu <skyline0705@gmail.com>
2023-04-06 21:49:20 +0800

    feat: 将examples改成SSR模式

--- packages/model/examples/client/entry-client.ts
+++ packages/model/examples/client/entry-client.ts
@@ -1,10 +1,8 @@
 import { createApp } from './main';
 
 const { app, router } = createApp();
-
 // wait until router is ready before mounting to ensure hydration match
 router.isReady().then(() => {
-    app.mount('#app')
-
+    app.mount('#app', true)
     console.log('hydrated')
 });
