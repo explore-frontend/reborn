bb61d22e579c5cc383754c21aac9fb7ad1c6f8cb
Skyline Yu <skyline0705@gmail.com>
2023-04-20 16:47:53 +0800

    feat: 把单测拆分一下，避免各种奇技淫巧的写法

---
+++ packages/model/src/__test__/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import ComponentA from './ComponentA.vue';
+import ComponentB from './ComponentB.vue';
+
+const show = ref(false);
+const parentShow = ref(true);
+
+function change() {
+    show.value = !show.value;
+}
+
+function toggle() {
+    parentShow.value = !parentShow.value;
+}
+</script>
+
+<template>
+    <div class="app" v-if="parentShow" @click="change" @keypress="toggle"><ComponentA v-if="show" /><ComponentB v-else /></div>
+</template>
---
+++ packages/model/src/__test__/ComponentB.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { TestModel } from './test-model';
+import { useModel } from '../index';
+
+const model = useModel(TestModel);
+
+function click() {
+    model.a.value++;
+}
+
+const c = computed(() => model.b.value);
+
+</script>
+
+<template>
+    <div class="b" @click="click">B: a: {{ model.a.value }} b: {{ c }}</div>
+</template>
