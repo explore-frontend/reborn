ccf0cc2e4958485946def90140adddcfc90e2917
WhimsFate <whimsfate@hotmail.com>
2023-08-22 15:03:35 +0800

    feat: 🎸 暴露 useQuery stream

--- packages/model/examples/client/model.ts
+++ packages/model/examples/client/model.ts
@@ -5,16 +5,20 @@ export const TestModel = createModel(() => {
     const a = ref(1);
     const b = computed(() => a.value * 2);
     const c = ref(1);
-    const { data } = useRestQuery<{result: 1, data: number}>({
+    const { data, stream$ } = useRestQuery<{result: 1, data: number}>({
         url: '/api',
         method: 'post',
         variables() {
             console.error('啦啦啦啦啦');
             return {
-                params: c.value
+                params: b.value
             };
         },
     });
+
+    stream$.subscribe(sub => {
+        console.log(sub)
+    })
 
     return {
         data,
