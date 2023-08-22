a973b24b7f49cbec831e178e05fcfe104296c06a
WhimsFate <whimsfate@hotmail.com>
2023-08-22 14:05:35 +0800

    fix: 🐛 修复 pool 不能停止的问题

--- packages/model/src/operations/core.ts
+++ packages/model/src/operations/core.ts
@@ -12,6 +12,7 @@ import type { RouteLocationNormalizedLoaded } from 'vue-router';
 import { reactive, computed, watch } from 'vue';
 import { fromWatch } from '../utils';
 import { Observable, merge, map } from 'rxjs';
+import { MODE } from '../const';
 
 export function isDef<T>(v: T): v is NonNullable<T> {
     return v !== undefined && v !== null;
@@ -59,26 +60,28 @@ export function generateQueryOptions<ModelType, DataType>(
     const variables$ = fromWatch(() => variables.value, { immediate: true }).pipe(map(i => RequestReason.setVariables));
     const pollInterval$ = new Observable<RequestReason.poll>(subscriber => {
         let timeout: ReturnType<typeof setTimeout>;
-        watch(() => pollInterval.value, (val, oldVal) => {
-            if (val === oldVal) {
-                return;
-            }
-            if (val <= 0) {
-                clearTimeout(timeout);
-                return;
-            }
 
-            poll();
-        }, { immediate: true })
+        const poll = () => {
+            if(MODE === 'SSR') {
+                return
+            }
+            clearTimeout(timeout);
+            if(pollInterval.value > 0) {
+                timeout = setTimeout(doInterval, pollInterval.value);
+            }
+        }
+
         const doInterval = () => {
             subscriber.next(RequestReason.poll);
             poll();
         }
 
-        const poll = () => {
-            clearTimeout(timeout);
-            setTimeout(doInterval, pollInterval.value);
-        }
+        watch(() => pollInterval.value, (val, oldVal) => {
+            if (val === oldVal) {
+                return;
+            }
+            poll();
+        }, { immediate: true })
     });
 
     const fetchQuery$ = merge(
